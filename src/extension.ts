b38f28c9ce085fac9622574bedd105c75d0875ab
Florian Rappl <florian.rappl@smapiot.com>
2020-07-05 22:31:11 +0200

    Initial release

--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,62 @@
 import * as vscode from 'vscode';
+import { resolve } from 'path';
+
+function execCommand(cmd: string | undefined) {
+  if (cmd) {
+    const term = vscode.window.createTerminal({
+      name: 'Piral',
+    });
+    term.sendText(cmd, true);
+    term.show(true);
+  }
+}
+
+function runCommand(cmd: string) {
+  const workspace = vscode.workspace.workspaceFolders?.[0];
+
+  if (!workspace) {
+    vscode.window.showErrorMessage('Require a workspace.');
+  } else {
+    const project = resolve(workspace.uri.path, 'package.json');
+
+    try {
+      const { scripts = {} } = __non_webpack_require__(project) || {};
+      const candidates = Object.keys(scripts).filter((m) => scripts[m].trim().startsWith(cmd));
+      const shellCommand =
+        candidates.length === 0 ? cmd : candidates.length === 1 ? scripts[candidates.pop() ?? ''] : undefined;
+
+      if (shellCommand !== undefined) {
+        execCommand(shellCommand);
+      } else {
+        vscode.window.showQuickPick(candidates).then(execCommand);
+      }
+    } catch (err) {
+      vscode.window.showErrorMessage(`Could not load the "package.json". Make sure the workspace is valid.`);
+    }
+  }
+}
 
 export function activate(context: vscode.ExtensionContext) {
-  //TODO const workspace = vscode.workspace.workspaceFolders?.[0];
-
   context.subscriptions.push(
     vscode.commands.registerCommand('vscode-piral.cli.pilet.debug', () => {
-      vscode.window.showInformationMessage('Debug Pilet!');
+      runCommand('pilet debug');
     }),
     vscode.commands.registerCommand('vscode-piral.cli.pilet.build', () => {
-      vscode.window.showInformationMessage('Build Pilet!');
+      runCommand('pilet build');
     }),
     vscode.commands.registerCommand('vscode-piral.cli.pilet.publish', () => {
-      vscode.window.showInformationMessage('Publish Pilet!');
+      runCommand('pilet publish');
     }),
     vscode.commands.registerCommand('vscode-piral.cli.piral.debug', () => {
-      vscode.window.showInformationMessage('Debug Piral!');
+      runCommand('piral debug');
     }),
     vscode.commands.registerCommand('vscode-piral.cli.piral.build', () => {
-      vscode.window.showInformationMessage('Build Piral!');
+      runCommand('piral build');
     }),
     vscode.commands.registerCommand('vscode-piral.cli.piral.declaration', () => {
-      vscode.window.showInformationMessage('Declaration Piral!');
+      runCommand('piral declaration');
     }),
   );
 }
 
-export function deactivate(context: vscode.ExtensionContext) {}
+export function deactivate(context: vscode.ExtensionContext) { }
